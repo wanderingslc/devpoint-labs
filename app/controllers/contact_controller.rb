5f19a8a0592c75a73b8bea4764817d3ac74a5f9d
Stan Martineau <wanderingslc@gmail.com>
2013-11-11 14:16:29 -0700

    class exercises

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,17 +1,1 @@
-class ContactController < ApplicationController
-  def new
-    @message = Message.new
-  end
-
-  def create
-    @message = Message.new(params[:message])
-    if @message.valid?
-      ContactMailer.contact_us(@message).deliver
-      redirect_to root_path, notice: "Thanks for contacting us. We'll be in touch soon!"
-    else
-      flash.now.alert = "Please correct the errors below"
-      render :new
-    end
-  end
-end
 
