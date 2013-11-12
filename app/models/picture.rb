8e05fca7d143f43ae4cc43a313b9dac7f6aae1e9
Ben Eggett <beneggett@gmail.com>
2013-11-11 18:36:17 -0700

    removes some sizes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,12 +3,10 @@ class Picture < ActiveRecord::Base
   attr_accessible :caption, :description, :image
   has_attached_file :image, 
                     styles: { 
-                      hd: "1200x1200>", 
                       large: "600x600>", 
                       medium: "300x300>", 
-                      small: "150x150>", 
-                      thumb: "100x100>", 
-                      mini: "24x24>" }
+                      thumb: "100x100>"
+                      }
 
   validates :image, presence: true
 end
