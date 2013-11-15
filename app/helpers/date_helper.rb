1021cb164d6b7175a31b0c928a57acd983973543
Ben Eggett <beneggett@gmail.com>
2013-11-15 16:27:34 -0700

    Adds date helper

---
+++ app/helpers/date_helper.rb
@@ -0,0 +1,10 @@
+module DateHelper
+  def human_date(date)
+    time_tag(date.to_date)
+  end
+
+  def human_date_time(date)
+    date.strftime("%A %B %d, %Y")
+  end  
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :pictures, as: :pictureable
-  attr_accessible :body, :published_at, :seo_description, :seo_title, :title
+  attr_accessible :body, :published_at, :seo_description, :seo_title, :title, :user_id
 end
