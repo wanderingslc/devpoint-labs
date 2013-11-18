8cd4f4e59a2a9379c9ac07a670f248ae7290fe90
Ben Eggett <beneggett@gmail.com>
2013-11-18 11:41:41 -0700

    Removes reduncy to travis by setting environment variables in travis.yml file, sets length > 30 characters

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,3 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 DevpointLabs::Application.config.secret_token = ENV['SECRET_TOKEN']
-DevpointLabs::Application.config.secret_token = "90e5e64c9691c5c659f44d818f8e761c716ff5f6cc29d9472567e274a2e2802c7189f2847a1477d3935cd339531b3404a231bb3d3296cd18e07e20ec2187a475" if Rails.env.test? # Adds this for Travis
